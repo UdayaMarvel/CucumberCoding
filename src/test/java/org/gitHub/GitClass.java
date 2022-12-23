af79f1d85e3a6509ab48751744ec81078d09fe08
Gobi7 <gobitech7@gmail.com>
2022-12-23 22:13:06 +0530

    Ajio pjt

--- src/test/java/org/gitHub/GitClass.java
+++ src/test/java/org/gitHub/GitClass.java
@@ -1,8 +1,33 @@
 package org.gitHub;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Test;
 
-public class GitClass {                                                                                                                                                                                                                                                           
-	
-public static void main(String[] args) {
-	System.out.println("test");
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitClass { 
+	public static WebDriver driver;
+	@Test
+	public  void test1() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https:/www.ajio.com/");
+	}
+	@Test
+	public void test2() {
+		WebElement search = driver.findElement(By.xpath("//a[@title='MEN']"));
+		search.click();
+	}
+	@Test
+	public void test3() {
+		driver.close();
+
+
+	}
 }
-}
+
